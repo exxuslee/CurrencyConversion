bdf02d1a9f71483c0fc1b508ab78122866baec97
Exxuslee <exxuslee@gmail.com>
2022-06-02 10:29:32 +0300

    :app

--- data/src/main/java/com/exxuslee/data/local/CardInfoDatabase.kt
+++ data/src/main/java/com/exxuslee/data/local/CardInfoDatabase.kt
@@ -9,7 +9,7 @@ import com.exxuslee.data.local.entities.PriceEntity
  * Created by Exxus Lee on 22/07/2020.
  */
 
-@Database(entities = [PriceEntity::class],version = 1,exportSchema = false)
-abstract class PriceDatabase : RoomDatabase(){
+@Database(entities = [PriceEntity::class], version = 1, exportSchema = false)
+abstract class PriceDatabase : RoomDatabase() {
     abstract val priceDao: PriceDao
 }
