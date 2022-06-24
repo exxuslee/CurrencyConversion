00c192cb5fae23fa56e6637e41e163bd87e1f84d
Exxuslee <exxuslee@gmail.com>
2022-06-24 13:20:02 +0300

    domain add saveAll

--- domain/src/main/java/com/exxuslee/domain/usecases/CurrenciesUseCase.kt
+++ domain/src/main/java/com/exxuslee/domain/usecases/CurrenciesUseCase.kt
@@ -5,13 +5,16 @@ import com.exxuslee.domain.repositories.PriceRepository
 import com.exxuslee.domain.utils.Result
 
 interface CurrenciesUseCase {
-    suspend fun load (getFromRemote: Boolean): Result<Symbols>
-    suspend fun save (symbols: Symbols)
+    suspend fun load(getFromRemote: Boolean): Result<Symbols>
+    suspend fun save(symbol: Symbols.Symbol)
+    suspend fun saveAll(symbols: Symbols)
 
     class Base(private val repository: PriceRepository) : CurrenciesUseCase {
         override suspend fun load(getFromRemote: Boolean): Result<Symbols> =
             repository.getCurrencies(getFromRemote)
 
-        override suspend fun save(symbols: Symbols) = repository.saveCurrencies(symbols)
+        override suspend fun save(symbol: Symbols.Symbol) = repository.saveCurrencies(symbol)
+
+        override suspend fun saveAll(symbols: Symbols) = repository.saveAllCurrencies(symbols)
     }
 }
